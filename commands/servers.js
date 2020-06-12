e7a8f7556694e6db1e52b99b0ab3a0cc3fccd059
sailesh10115 <50526710+sailesh10115@users.noreply.github.com>
2020-06-12 12:43:58 -0400

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -3,10 +3,16 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
     let guilds = ""
-    bot.guilds.forEach(guild => {
-        guilds += `${guild.name} - ${guild.memberCount}\n`
+    bot.guilds.cache.forEach(async guild => {
+/*        let channel = guild.channels.cache.filter(c=>c.type === "text" && c.permissionsFor(guild.me) && c.permissionsFor(guild.me).has("CREATE_INVITE")).random();
+        let invite = "Nope!"
+        if(channel) invite = `discord.gg/${(await channel.createInvite()).code}` */
+        let log = `${guild.name} - ${guild.id} - ${guild.memberCount}`
+        guilds += `${log}\n`
     })
-    message.channel.send(`${guilds}`)
+    message.channel.send(`${guilds}`).catch(e=>console.log(e.message))
+    console.log(`${guilds}`)
+
 }
 //name this whatever the command name is. 
 module.exports.help = {
